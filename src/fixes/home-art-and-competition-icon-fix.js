e8c3e7d9532947e48fc8e31fa4c16b97ac9a461a
Juliacmm <juliacezarmm@gmail.com>
2026-08-14 13:41:20 -0300

    Atualiza cache da Home Código Fixa 7

--- src/fixes/home-art-and-competition-icon-fix.js
+++ src/fixes/home-art-and-competition-icon-fix.js
@@ -153,7 +153,7 @@
   if (document.getElementById('fixaHomeGoalsStreakProtectionV1Loader')) return;
   const script = document.createElement('script');
   script.id = 'fixaHomeGoalsStreakProtectionV1Loader';
-  script.src = 'src/fixes/home-goals-streak-protection-v1.js?v=20260814-streak-goals-v1';
+  script.src = 'src/fixes/home-goals-streak-protection-v1.js?v=20260814-streak-goals-v2';
   script.defer = true;
   document.head.appendChild(script);
 })();
